7e204b63cb74834756153e95e8d26876f015f4dd
einarno@hotmail.com <einarno@hotmail.com>
2024-07-14 16:33:25 +0200

    2.5 Add expressing parsing

--- parser/parser.go
+++ parser/parser.go
@@ -56,6 +56,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -99,4 +101,14 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.PeekError(t)
 		return false
 	}
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
 }
